3d5278f7c452ca594a62044466e0f35b776330a3
Pavel Kiselev <sumere4ny@gmail.com>
2021-08-09 23:32:50 +0300

    Update login controller

--- controllers/users.js
+++ controllers/users.js
@@ -83,7 +83,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({ message: SUCCESS });
+        .send({ _id: user._id, message: SUCCESS });
     })
     .catch(next);
 };
