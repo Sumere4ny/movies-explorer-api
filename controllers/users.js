c8ff9029a387d9ab2c33861061a7f18c99d035e2
Pavel Kiselev <sumere4ny@gmail.com>
2021-08-10 11:20:41 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -81,7 +81,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          secure: true,
+          sameSite: false,
         })
         .send({ _id: user._id, message: SUCCESS });
     })
