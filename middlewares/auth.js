9f41b661bb0634de67204d41cb57f7a04bf3025d
Pavel Kiselev <sumere4ny@gmail.com>
2021-08-10 12:15:12 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,11 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-
+  
+  if (!token) {
+    throw new UnauthorizedError('Необходимо залогиниться');
+  }
+  
   let payload;
   try {
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
